772acbf96d497d2bb24a8fe4f7d0aabcb49be4b8
Kartik Punna <kartikpunna@gmail.com>
2026-04-27 19:55:40 +1000

    implement functionality for emailing (currently made for gmail)

---
+++ api/services/email_service.py
@@ -0,0 +1,37 @@
+import smtplib
+import os
+from email.message import EmailMessage
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+def require_env(name: str) -> str:
+    value = os.getenv(name)
+    if not value:
+        raise ValueError(f"Missing environment variable: {name}")
+    return value
+
+
+EMAIL_FROM = require_env("EMAIL_FROM")
+EMAIL_PASSWORD = require_env("EMAIL_PASSWORD")
+SMTP_HOST = require_env("SMTP_HOST")
+SMTP_PORT = int(os.getenv("SMTP_PORT", 465))
+
+
+def send_email(to_email: str, subject: str, body: str):
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_FROM
+    msg["To"] = to_email
+    msg.set_content(body)
+
+    with smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT) as smtp:
+        #smtp.starttls()
+        print(SMTP_HOST, SMTP_PORT)
+        smtp.login(EMAIL_FROM, EMAIL_PASSWORD)
+        smtp.send_message(msg)
+
+
+send_email("kartikpunna5@gmail.com", "Test Email", "This works")
