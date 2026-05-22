9bd0166a54ff9a818fcff914448e1e8f674a9dd8
Kartik Punna <kartikpunna@gmail.com>
2026-05-22 12:04:56 +1000

    added config.py file

---
+++ api/config.py
@@ -0,0 +1,15 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+    database_url: str
+    frontend_url: str = "http://localhost:8000"
+
+
+settings = Settings() # type: ignore[call-arg] # Loaded from .env file
